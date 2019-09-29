a31eea5d51ff100af0b4d4b72703bca7070b4fa0
IWriteCuteBUG <1052299507@qq.com>
2019-09-29 21:38:44 +0800

    ssh

--- mall/src/main/java/com/cskaoyan/mall/controller/HelloController.java
+++ mall/src/main/java/com/cskaoyan/mall/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("hello")
     public String hello() {
-        return "hello LiJiaWei666";
+        return "hello LiJiaWei666111111";
     }
 }
