1762335094c9e5bc5a4e72e522c9a37484e6a5ba
IWriteCuteBUG <1052299507@qq.com>
2019-09-30 01:55:47 +0800

    深夜提交怕不怕

--- mall/src/main/java/com/cskaoyan/mall/MallApplication.java
+++ mall/src/main/java/com/cskaoyan/mall/MallApplication.java
@@ -1,9 +1,11 @@
 package com.cskaoyan.mall;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cskaoyan.mall.mapper")
 public class MallApplication {
 
     public static void main(String[] args) {
