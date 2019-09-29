bc68346be8c143ab9bb100b007c5c0940b06da78
IWriteCuteBUG <1052299507@qq.com>
2019-09-29 21:42:09 +0800

    1111

--- mall/src/main/java/com/cskaoyan/mall/controller/HelloController.java
+++ mall/src/main/java/com/cskaoyan/mall/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("hello")
+    @RequestMapping("hello1")
     public String hello() {
         return "hello LiJiaWei666111111";
     }
