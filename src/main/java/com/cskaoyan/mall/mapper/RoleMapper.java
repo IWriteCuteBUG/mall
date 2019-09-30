48fc24d47fd15de8808167b20cb53b97c04facc6
lijiawei199 <986809553@qq.com>
2019-09-30 17:31:13 +0800

    逆向!!

---
+++ src/main/java/com/cskaoyan/mall/mapper/RoleMapper.java
@@ -0,0 +1,30 @@
+package com.cskaoyan.mall.mapper;
+
+import com.cskaoyan.mall.bean.Role;
+import com.cskaoyan.mall.bean.RoleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface RoleMapper {
+    long countByExample(RoleExample example);
+
+    int deleteByExample(RoleExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    List<Role> selectByExample(RoleExample example);
+
+    Role selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByExample(@Param("record") Role record, @Param("example") RoleExample example);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
---
+++ src/main/java/com/cskaoyan/mall/mapper/StorageMapper.java
@@ -0,0 +1,30 @@
+package com.cskaoyan.mall.mapper;
+
+import com.cskaoyan.mall.bean.Storage;
+import com.cskaoyan.mall.bean.StorageExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface StorageMapper {
+    long countByExample(StorageExample example);
+
+    int deleteByExample(StorageExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Storage record);
+
+    int insertSelective(Storage record);
+
+    List<Storage> selectByExample(StorageExample example);
+
+    Storage selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Storage record, @Param("example") StorageExample example);
+
+    int updateByExample(@Param("record") Storage record, @Param("example") StorageExample example);
+
+    int updateByPrimaryKeySelective(Storage record);
+
+    int updateByPrimaryKey(Storage record);
+}
