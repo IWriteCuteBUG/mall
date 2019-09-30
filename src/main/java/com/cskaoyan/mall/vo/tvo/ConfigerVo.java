192ac7c81d38ee5b86badc5553ecd481c7f58ace
StoryOfTangSong <804911919@qq.com>
2019-09-30 19:12:57 +0800

    已完善

---
+++ src/main/java/com/cskaoyan/mall/vo/tvo/ConfigerVo.java
@@ -0,0 +1,40 @@
+package com.cskaoyan.mall.vo.tvo;
+
+public class ConfigerVo {
+     private String litemall_mall_phone;
+    private String litemall_mall_address;
+    private String litemall_mall_name;
+    private String litemall_mall_qq;
+
+    public String getLitemall_mall_phone() {
+        return litemall_mall_phone;
+    }
+
+    public void setLitemall_mall_phone(String litemall_mall_phone) {
+        this.litemall_mall_phone = litemall_mall_phone;
+    }
+
+    public String getLitemall_mall_address() {
+        return litemall_mall_address;
+    }
+
+    public void setLitemall_mall_address(String litemall_mall_address) {
+        this.litemall_mall_address = litemall_mall_address;
+    }
+
+    public String getLitemall_mall_name() {
+        return litemall_mall_name;
+    }
+
+    public void setLitemall_mall_name(String litemall_mall_name) {
+        this.litemall_mall_name = litemall_mall_name;
+    }
+
+    public String getLitemall_mall_qq() {
+        return litemall_mall_qq;
+    }
+
+    public void setLitemall_mall_qq(String litemall_mall_qq) {
+        this.litemall_mall_qq = litemall_mall_qq;
+    }
+}
