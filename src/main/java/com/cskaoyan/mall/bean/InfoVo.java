5405fff016254e888f6b8c527e8ab56ffbf476be
StoryOfTangSong <804911919@qq.com>
2019-09-30 17:37:57 +0800

    修正

---
+++ src/main/java/com/cskaoyan/mall/bean/InfoVo.java
@@ -0,0 +1,42 @@
+package com.cskaoyan.mall.bean;
+
+import java.util.List;
+
+public class InfoVo {
+    private String avatar;
+    private String name;
+    private List perms;
+    private List roles;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List getPerms() {
+        return perms;
+    }
+
+    public void setPerms(List perms) {
+        this.perms = perms;
+    }
+
+    public List getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List roles) {
+        this.roles = roles;
+    }
+}
