cec44b5ca141443f43e968e13832f5d04ac9beff
IWriteCuteBUG <1052299507@qq.com>
2019-10-07 21:22:23 +0800

    团购修正

--- src/main/java/com/cskaoyan/mall/vo/wechatvo/zyp/MyGrounpVo.java
+++ src/main/java/com/cskaoyan/mall/vo/wechatvo/zyp/MyGrounpVo.java
@@ -16,7 +16,7 @@ public class MyGrounpVo {
     private Integer orderId;
     private String orderSn;
     private BigDecimal actualPrice;
-    private Integer joinerPrice;
+    private Integer joinerCount;
     private List<OrderGoods> goodsList;
     private GrouponRules rules;
     private Integer id;
@@ -33,18 +33,6 @@ public class MyGrounpVo {
 
     public String getCreator() {
         return creator;
-    }
-
-    public void setCreator(Boolean creator) {
-        isCreator = creator;
-    }
-
-    public HandleOption getHandleOption() {
-        return handleOption;
-    }
-
-    public void setHandleOption(HandleOption handleOption) {
-        this.handleOption = handleOption;
     }
 
     public void setCreator(String creator) {
@@ -83,12 +71,12 @@ public class MyGrounpVo {
         this.actualPrice = actualPrice;
     }
 
-    public Integer getJoinerPrice() {
-        return joinerPrice;
+    public Integer getJoinerCount() {
+        return joinerCount;
     }
 
-    public void setJoinerPrice(Integer joinerPrice) {
-        this.joinerPrice = joinerPrice;
+    public void setJoinerCount(Integer joinerCount) {
+        this.joinerCount = joinerCount;
     }
 
     public List<OrderGoods> getGoodsList() {
@@ -115,5 +103,19 @@ public class MyGrounpVo {
         this.id = id;
     }
 
+    public Boolean getIsCreator() {
+        return isCreator;
+    }
 
+    public void setIsCreator(Boolean isCreator) {
+        this.isCreator = isCreator;
+    }
+
+    public HandleOption getHandleOption() {
+        return handleOption;
+    }
+
+    public void setHandleOption(HandleOption handleOption) {
+        this.handleOption = handleOption;
+    }
 }
