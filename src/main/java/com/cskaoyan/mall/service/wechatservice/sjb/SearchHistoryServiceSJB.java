bc09648ca322096bd0a150e9bfb6f6f86a0be644
jabsun <jabsun@163.com>
2019-10-08 16:57:47 +0800

    searchhistory bug fix

--- src/main/java/com/cskaoyan/mall/service/wechatservice/sjb/SearchHistoryServiceSJB.java
+++ src/main/java/com/cskaoyan/mall/service/wechatservice/sjb/SearchHistoryServiceSJB.java
@@ -2,10 +2,13 @@ package com.cskaoyan.mall.service.wechatservice.sjb;
 
 import com.cskaoyan.mall.bean.SearchHistory;
 
+import java.util.Date;
 import java.util.List;
 
 public interface SearchHistoryServiceSJB {
     List<SearchHistory> querySearchHistoryByUserId(int userId);
 
     int addSearchHistory(SearchHistory searchHistory);
+
+    int updateSearchhistoryUpdatetimeByKeyword(String keyword, Date updatetime);
 }
